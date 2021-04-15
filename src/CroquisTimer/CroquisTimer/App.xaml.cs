0c00777e9f1d46ebce9de779d3781508e3b77e0e
JJToki <44167532+JJToki@users.noreply.github.com>
2021-04-15 23:38:23 +0900

    Add solution

---
+++ src/CroquisTimer/CroquisTimer/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CroquisTimer.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace CroquisTimer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
