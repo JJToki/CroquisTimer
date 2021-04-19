11a799cfcae954e647d3da7cc3ab787ff8ead7b1
JJToki <44167532+JJToki@users.noreply.github.com>
2021-04-19 19:36:57 +0900

    Implement ViewModel

--- src/CroquisTimer/CroquisTimer/ViewModels/MainWindowViewModel.cs
+++ src/CroquisTimer/CroquisTimer/ViewModels/MainWindowViewModel.cs
@@ -1,20 +1,40 @@
-﻿using System.Reflection;
+﻿using System.Reactive.Disposables;
+using System.Reflection;
+using CroquisTimer.Models;
 using Prism.Mvvm;
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace CroquisTimer.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        #region Field
+        CompositeDisposable _disposables;
+        CountDownTimer _timer;
+        #endregion
+
         #region Property
         public ReactiveProperty<string> Title { get; }
+        public ReactiveProperty<string> TimeRemainingText { get; }
+        #endregion
+
+        #region Command
+        public ReactiveCommand StartTimerCommand { get; } = new ReactiveCommand();
         #endregion
 
         #region Method
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public MainWindowViewModel()
         {
+            _timer = new CountDownTimer();
+
             var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
-            Title = new ReactiveProperty<string>(assemblyName);
+            Title = new ReactiveProperty<string>(assemblyName).AddTo(_disposables);
+
+            StartTimerCommand.Subscribe(() => _timer.Start());
         }
         #endregion
     }
